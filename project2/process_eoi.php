c9af7fda35f120f1b94668906a582effd493f06b
Bigteaforfolks <105914449@student.swin.edu.au>
2025-05-25 01:06:57 +1000

    Base EOI Processing HTML and including header and footer (.inc)

---
+++ project2/process_eoi.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Job Application page of the Rainy Season website.">
+    <meta name="keywords" content="Rainy Season, Jobs, Apply, Application, Work, Form">
+    <meta name="author" content="Jacob Ang">
+    <title>Rainy Season EOI Success Page</title>
+
+    <!-- Reference to external CSS file -->
+    <link href="styles/styles.css" rel="stylesheet">
+</head>
+<body>
+<?php
+
+    // include header
+    include_once("header.inc");
+
+    
+    // include footer
+    include_once("footer.inc");
+?>
+</body>
