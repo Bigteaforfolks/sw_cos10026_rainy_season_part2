0c5caf18937cd0c2d51cbd633d351b45f4fdb442
Bigteaforfolks <105914449@student.swin.edu.au>
2025-05-25 17:57:10 +1000

    Base file for manage.php and eoi_search_result.php created/modified.

--- project2/eoi_search_result.php
+++ project2/eoi_search_result.php
@@ -2,23 +2,21 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="Manage Page for EOI Table of Database.">
+    <meta name="description" content="Results of EOI Table Search from Database.">
     <meta name="keywords" content="Rainy Season, Jobs, Apply, Application, Work">
     <meta name="author" content="Jacob Ang">
-    <title>Rainy Season Manage Page</title>
+    <title>Rainy Season EOI Search Result</title>
 
     <!-- Reference to external CSS file -->
     <link href="styles/styles.css" rel="stylesheet">
 </head>
 
-<body id="manage-page">
+<body id="eoi-search-result-page">
 
     <?php
-        include 'header.inc';
-    ?>
+        include "header.inc";
 
-    <?php
-        include 'footer.inc';
+        include "footer.inc";
     ?>
 
 </body>
